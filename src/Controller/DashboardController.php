53ea3e5e8e92ec5d776ba06ec913651a64584cb9
agdc101 <agdc101@gmail.com>
2023-04-04 19:16:57 +0100

    dashboard content added

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,21 @@ class DashboardController extends AbstractController
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $client = HttpClient::create();
+        $response = $client->request('GET', $this->getParameter('llama_api'));
+        if ($response->getStatusCode() !== 200) {
+            return $this->render('dashboard/index.html.twig');
+        }
+        // convert to array and get data.
+        $responseData = $response->toArray()['data'];
+        // get last/most recent object and extract apy
+        $responseApy = end($responseData)['apy']*$this->getParameter('commission');
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'user' => $this->getUser()->getFirstName(),
+            'apy' => $responseApy
         ]);
     }
 }
