084025836076134c7455b8815de77b0ce683de73
agdc101 <agdc101@gmail.com>
2023-04-04 17:47:03 +0100

    fetch apy data from defi-llama. api added as yaml variable

--- src/Controller/RootController.php
+++ src/Controller/RootController.php
@@ -5,12 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
+
+
 
 class RootController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
+
     public function index(): Response
     {
-        return $this->render('homepage/index.html.twig');
+        $client = HttpClient::create();
+        $response = $client->request('GET', $this->getParameter('llama_api'));
+
+        if ($response->getStatusCode() !== 200) {
+            return $this->render('dashboard/index.html.twig');
+        }
+
+        // convert to array and get data.
+        $responseData = $response->toArray()['data'];
+        // get last/most recent object and extract apy
+        $responseApy = end($responseData)['apy']*0.80;
+
+        return $this->render('homepage/index.html.twig', ['apy' => $responseApy]);
+
     }
 }
