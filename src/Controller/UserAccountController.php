385fa1f8fa55a8593aa5072602a95e8366604d3f
agdc101 <agdc101@gmail.com>
2023-04-11 18:53:16 +0100

    close account route added to user route

--- src/Controller/UserAccountController.php
+++ src/Controller/UserAccountController.php
@@ -30,4 +30,15 @@ class UserAccountController extends AbstractController
             'updateUserForm' => $form->createView(),
         ]);
     }
+    #[Route('/close-user-account', name: 'app_close_user_account')]
+    public function close(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('account_closed', 'Sorry to see you go. Your account has been successfully closed.');
+
+        return $this->render('homepage/index.html.twig');
+    }
 }
