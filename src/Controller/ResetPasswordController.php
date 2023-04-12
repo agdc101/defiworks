4c4e39238f11d14b6a39f078aa2cd9111f63ca9e
agdc101 <agdc101@gmail.com>
2023-04-12 19:36:20 +0100

    flash message added to dash after pass reset. flash message code title change

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -119,6 +119,8 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
+            $this->addFlash('password_reset_success', 'You have successfully changed your password.');
+
             return $this->redirectToRoute('app_login');
         }
 
