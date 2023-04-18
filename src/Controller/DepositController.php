550f7cd83765f1a266b4b71e1fa055a95f57dca9
agdc101 <agdc101@gmail.com>
2023-04-18 17:45:05 +0100

    DI link added

--- src/Controller/DepositController.php
+++ src/Controller/DepositController.php
@@ -11,8 +11,6 @@ class DepositController extends AbstractController
     #[Route('/deposit', name: 'app_deposit')]
     public function index(): Response
     {
-        return $this->render('deposit/deposit.html.twig', [
-            'controller_name' => 'DepositController',
-        ]);
+        return $this->render('deposit/deposit.html.twig');
     }
 }
