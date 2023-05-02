710a78fdbd44d067252e686175050aac9350b7c8
agdc101 <agdc101@gmail.com>
2023-05-02 13:09:56 +0100

    env changes. deposit component refactored. now checks for 00 decimals

--- assets/js/react/DepositInputs.jsx
+++ assets/js/react/DepositInputs.jsx
@@ -2,15 +2,14 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 
 let SUSDRate = 0;
-let fee = 0.985;
+let fee = 0.9875;
 const GECKO_API = 'https://api.coingecko.com/api/v3/simple/token_price/optimistic-ethereum?contract_addresses=0x8c6f28f2f1a3c87f0f938b96d27520d9751ec8d9&vs_currencies=gbp';
 
 const DepositInputs = () =>  {
     // state variables
     const [gbpDepositAmount, setGbpDepositAmount] = useState('');
     const [usdDepositAmount, setUsdDepositAmount] = useState('');
-    const [isGbpValid, setIsGbpValid] = useState(true);
-    const [isUsdValid, setIsUsdValid] = useState(false);
+    const [isGbpValid, setIsGbpValid] = useState(false);
 
     // function that gets gbp->susd rate from coingecko api
     async function getRate() {
@@ -20,28 +19,23 @@ const DepositInputs = () =>  {
     }
 
     // function that checks if the USD amount is valid
-    function checkUsdIsValid(value) {
-        console.log('usd: ' + value);
-        console.log(typeof value);
-        {(value > 0 || value !== '') ? setIsUsdValid(true) : setIsUsdValid(false)};
+    function checkGbpIsValid(value) {
+        {(value > 20) ? setIsGbpValid(true) : setIsGbpValid(false)};
     }
 
-    //validate input
+    //validate input, regex check for letters etc, remove commas from the value, then format the value to have the correct commas
     function validateAndSetAllInputs(value, currEvent) {
         const pattern = /^[0-9,. ]*$/;
         if (!pattern.test(value)) return;
-        // check if the usd value is valid
-        checkUsdIsValid(value);
-        // remove commas from the value
+
         let strippedVal = value.replace(/,/g, '');
-        // format the value to have the correct commas
         let formattedValue = Number(strippedVal).toLocaleString();
 
         if (currEvent === 'gbp') {
-            setGbpDepositAmount(formattedValue);
+            (formattedValue === '0') ? setGbpDepositAmount('') : setGbpDepositAmount(formattedValue);
             setOtherInput(strippedVal, 'gbp');
         } else {
-            setUsdDepositAmount(formattedValue);
+            (formattedValue === '0') ? setUsdDepositAmount('') : setUsdDepositAmount(formattedValue);
             setOtherInput(strippedVal, 'usd');
         }
     }
@@ -49,25 +43,29 @@ const DepositInputs = () =>  {
     // function that handles the click event on the continue button, adding gbp amount to the url
     function ConfirmDepositHandler(event) {
         event.preventDefault();
-        if (isUsdValid) {
+        if (isGbpValid) {
             window.location.href = '/deposit/confirm-deposit/GbpAmt=' + gbpDepositAmount + '&UsdAmt=' + usdDepositAmount;
         }
     }
 
-    // helper function that sets the value of the other input field
-    function setOtherInput(value, curr) {
-        if (curr === 'gbp') {
-            // calculate the usd sum and set to number.
-            let usdSum = Number(((value / SUSDRate) * fee));
-            // format the value to have the correct commas
-            let formattedValue = usdSum.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    // function that formats the value to have the correct commas and decimal places
+    function formatValue(value) {
+        if (value.toString().slice(-3) === ".00") {
+            return Number(value).toLocaleString('en-US', { maximumFractionDigits: 0 });
+        } else {
+            return Number(value).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        }
+    }
+
+    function setOtherInput(value, currency) {
+        if (currency === 'gbp') {
+            let usdSum = ((value / SUSDRate) * fee);
+            let formattedValue = formatValue(usdSum);
             (usdSum === 0) ? setUsdDepositAmount('') : setUsdDepositAmount(formattedValue);
         } else {
-            let gbpSum = Number(((value * SUSDRate) / fee));
-            let formattedValue = gbpSum.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-            // check if the value is less than 10, if so set to invalid.
-            (gbpSum < 20) ? setIsGbpValid(false) : setIsGbpValid(true);
-            // if 0, set to empty string
+            let gbpSum = ((value * SUSDRate) / fee);
+            checkGbpIsValid(gbpSum);
+            let formattedValue = formatValue(gbpSum);
             (gbpSum === 0) ? setGbpDepositAmount('') : setGbpDepositAmount(formattedValue);
         }
     }
@@ -79,8 +77,7 @@ const DepositInputs = () =>  {
 
     // event handlers for the two input fields
     function setGbpDepositAmountHandler(event) {
-        // check if the value is less than 20, if so set to invalid.
-        (event.target.value < 20) ? setIsGbpValid(false) : setIsGbpValid(true);
+        checkGbpIsValid(event.target.value);
         validateAndSetAllInputs(event.target.value, 'gbp');
     }
 
@@ -98,8 +95,13 @@ const DepositInputs = () =>  {
                 <label htmlFor="UsdDepositAmount">Deposit Amount In USD($)</label>
                 <input type="text" id="UsdDepositAmount" name="UsdDepositAmount" maxLength="6" onChange={setUsdDepositAmountHandler} value={usdDepositAmount}/>
             </form>
-            {isUsdValid ? <p>Your account balance will be ${usdDepositAmount}</p> : <p>Please enter a deposit amount</p>}
-            <a id="confirm-continue-btn" href="/deposit/confirm-deposit" onClick={ConfirmDepositHandler}>Continue</a>
+            {isGbpValid ?
+                <div>
+                    <p>Your account balance will be ${usdDepositAmount}</p>
+                    <a id="confirm-continue-btn" href="/deposit/confirm-deposit" onClick={ConfirmDepositHandler} >Continue</a>
+                </div>
+                :
+                <p>Please enter a deposit amount</p>}
         </div>
     );
 }
