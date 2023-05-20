2a8f4b538b77a85ee4fde721725ca667f57392df
agdc101 <agdc101@gmail.com>
2023-05-20 07:53:21 +0100

    deposit inputs refactored

--- src/Controller/DepositController.php
+++ src/Controller/DepositController.php
@@ -33,66 +33,8 @@ class DepositController extends AbstractController
             ]);
         }
 
-        $session = $request->getSession();
-        $deposit = new Deposits();
-
-        $form = $this->createForm(AddPendingDepositType::class, $deposit);
-        $form->handleRequest($request);
-
-        //if session variable gbpDeposit is set
-        if ($session->get('gbpDeposit') && $session->get('usdDeposit')) {
-
-            $cleanGbp = str_replace(',', '', $session->get('gbpDeposit'));
-            $cleanUsd = str_replace(',', '', $session->get('usdDeposit'));
-
-            try {
-                //set default values for deposit
-                $deposit
-                    ->setIsVerified(false)
-                    ->setTimestamp(new DateTimeImmutable('now', new \DateTimeZone('Europe/London')))
-                    ->setUserEmail($this->getUser()->getEmail())
-                    ->setUserId($this->getUser()->getId())
-                    ->setUsdAmount($cleanUsd)
-                    ->setGbpAmount($cleanGbp);
-            } catch (Exception) {
-                return $this->render('error/error.html.twig');
-            }
-        }
-
-        //if form is submitted and valid
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($deposit);
-            $entityManager->flush();
-            try {
-                //send email to admin to confirm deposit
-                list($firstName, $lastName, $userEmail) = [$this->getUser()->getFirstName(), $this->getUser()->getLastName(), $this->getUser()->getEmail()];
-                list($gbpAmount, $date, $depositId) = [$deposit->getGbpAmount(), $deposit->getTimestamp(), $deposit->getId()];
-                $dateString = $date->format('H:i:s Y-m-d');
-                $email = (new Email())
-                    ->from('admin@defiworks.co.uk')
-                    ->to('admin@defiworks.co.uk')
-                    ->subject('New Deposit - Confirmation required')
-                    ->html("$firstName $lastName ($userEmail) has made a new deposit of £$gbpAmount at $dateString <br><br> confirm by going to <a href='http://localhost:8000/admin/confirm-deposit/$depositId'>https://defiworks.co.uk/admin/confirm-deposit/$depositId</a>");
-                $mailer->send($email);
-
-            } catch ( TransportExceptionInterface | Exception) {
-                return $this->render('error/error.html.twig');
-            }
-            //add flash message
-            $this->addFlash('gbp_amount', $gbpAmount);
-            //redirect to deposit confirmation page
-            return $this->redirectToRoute('app_deposit_confirmed');
-        }
         //render deposit template - if form is not submitted
-        return $this->render('deposit/deposit.html.twig', [
-            'AddPendingDepositForm' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/deposit/deposit-confirmation', name: 'app_deposit_confirmed')]
-    public function renderDepositConfirmationTemplate(): Response
-    {
-        return $this->render('deposit_confirmation/deposit-confirmation.html.twig');
+        return $this->render('deposit/deposit.html.twig');
     }
 
     #[Route('/create-deposit-session', methods: ['POST'])]
@@ -127,20 +69,70 @@ class DepositController extends AbstractController
     }
 
     #[Route('/deposit/deposit-details', name: 'app_deposit_details')]
-    public function renderDepositDetailsTemplate(Request $request): Response
+    public function renderDepositDetailsTemplate(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         //get usdDeposit and gbpDeposit from session
         $session = $request->getSession();
-        $gbpDeposit = $session->get('gbpDeposit');
         $deposit = new Deposits();
+        $gbpDeposit = $session->get('gbpDeposit');
 
         $form = $this->createForm(AddPendingDepositType::class, $deposit);
         $form->handleRequest($request);
+
+        //if form is submitted and valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            //if session variable gbpDeposit is set
+            if ($session->get('gbpDeposit')) {
+
+                $cleanGbp = str_replace(',', '', $session->get('gbpDeposit'));
+                $cleanUsd = str_replace(',', '', $session->get('usdDeposit'));
+                try {
+                    //set default values for deposit
+                    $deposit
+                        ->setIsVerified(false)
+                        ->setTimestamp(new DateTimeImmutable('now', new \DateTimeZone('Europe/London')))
+                        ->setUserEmail($this->getUser()->getEmail())
+                        ->setUserId($this->getUser()->getId())
+                        ->setGbpAmount($cleanGbp)
+                        ->setUsdAmount($cleanUsd);
+                } catch (Exception) {
+                    return $this->render('error/error.html.twig');
+                }
+            }
+
+            $entityManager->persist($deposit);
+            $entityManager->flush();
+            try {
+                //send email to admin to confirm deposit
+                list($firstName, $lastName, $userEmail) = [$this->getUser()->getFirstName(), $this->getUser()->getLastName(), $this->getUser()->getEmail()];
+                list($gbpAmount, $usdAmount, $date, $depositId) = [$deposit->getGbpAmount(), $deposit->getUsdAmount(), $deposit->getTimestamp(), $deposit->getId()];
+                $dateString = $date->format('H:i:s Y-m-d');
+                $email = (new Email())
+                    ->from('admin@defiworks.co.uk')
+                    ->to('admin@defiworks.co.uk')
+                    ->subject('New Deposit - Confirmation required')
+                    ->html("$firstName $lastName ($userEmail) has made a new deposit of £$gbpAmount ($$usdAmount) at $dateString <br><br> confirm by going to <a href='http://localhost:8000/admin/confirm-deposit/$depositId'>https://defiworks.co.uk/admin/confirm-deposit/$depositId</a>");
+                $mailer->send($email);
+
+            } catch ( TransportExceptionInterface | Exception) {
+                return $this->render('error/error.html.twig');
+            }
+            //add flash message
+            $this->addFlash('gbp_amount', $gbpAmount);
+            //redirect to deposit confirmation page
+            return $this->redirectToRoute('app_deposit_confirmed');
+        }
 
         return $this->render('deposit-details/deposit-details.html.twig', [
             'gbpDeposit' => $gbpDeposit,
             'AddPendingDepositForm' => $form->createView()
         ]);
+    }
+
+    #[Route('/deposit/deposit-confirmation', name: 'app_deposit_confirmed')]
+    public function renderDepositConfirmationTemplate(): Response
+    {
+        return $this->render('deposit_confirmation/deposit-confirmation.html.twig');
     }
 
 }
