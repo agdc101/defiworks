142c0c05dcda6c6cb2b12e743f81f58622211e03
agdc101 <agdc101@gmail.com>
2023-05-31 16:46:41 +0100

    new forms added to replace input comp

--- src/Form/WithdrawDetailsType.php
+++ src/Form/WithdrawDetailsType.php
@@ -3,16 +3,51 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class WithdrawDetailsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('sort_code')
-            ->add('account_number')
+            ->add('sort_code', TextType::class, [
+                'label' => 'Sort Code',
+                'attr' => [
+                    'placeholder' => '00-00-00'
+                ],
+                 'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a sort code',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Your sort code should be {{ limit }} characters long, including hyphens',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 8,
+                    ]),
+                ],
+            ])
+            ->add('account_number', TextType::class, [
+                'label' => 'Account Number',
+                'attr' => [
+                    'placeholder' => '0123456789'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an account number',
+                    ]),
+                    new Length([
+                        'min' => 9,
+                        'minMessage' => 'Your account number should be {{ limit }} characters long',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 9,
+                    ]),
+                ],
+            ])
         ;
     }
 
