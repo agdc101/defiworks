7aa9938747dee7d5ec5313b48ce87813484e563e
agdc101 <agdc101@gmail.com>
2023-06-07 12:15:49 +0100

    script updated - connected to db

--- config/app.php
+++ config/app.php
@@ -2,13 +2,25 @@
 $servername = "localhost";
 $username = "root";
 $password = "jupiter68";
+$dbname = "defiworks";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$sql = "SELECT * FROM Deposits";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo " " . $row['usd_amount'] . " ";
+    }
+} else {
+    echo "No users found";
+}
 
