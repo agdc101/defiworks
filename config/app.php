ceb8e5052e7bc49ad9c8156f431cf6a7d7a6a92b
agdc101 <agdc101@gmail.com>
2023-06-08 14:59:18 +0100

    deposits made same day excluded

--- config/app.php
+++ config/app.php
@@ -16,15 +16,15 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-
 $url = "https://yields.llama.fi/chart/b65aef64-c153-4567-9d1a-e0040488f97f";
 
+// Initialize cURL
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
+// Execute the cURL request
 $response = curl_exec($curl);
 curl_close($curl);
-//default is 3% if no APY is returned from API
 
 if ($response !== false) {
     $data = json_decode($response, true);
@@ -49,28 +49,37 @@ if ($response !== false) {
     echo "Failed to retrieve data.";
 }
 
+//get the average APY of $nexAPY and $responseApy
+$totalApy = (($apyValue + $nexApy) / 2) * 0.85;
+$dailyApy = $totalApy / 365;
 
+//for each user in the database
+$userSql = "SELECT * FROM User";
+$userResult = $conn->query($userSql);
 
-//select all deposit records with a timestamp of today
-$depositSql = "SELECT * FROM Deposits WHERE DATE(timestamp) = CURDATE()";
-$depositResult = $conn->query($depositSql);
+//for each user in $userResult
+while ($userRow = $userResult->fetch_assoc()) {
+    //get the user's balance
+    $userID = $userRow['id'];
 
-//convert $depositResult to array
-$depositArray = $depositResult->fetch_all(MYSQLI_ASSOC);
-//echo each deposit record
-foreach ($depositArray as $deposit) {
-    echo " - usd_amount: " . $deposit["usd_amount"] . " - timestamp: " . $deposit["timestamp"];
-    //add dailyApy to each deposit record
+    //get all deposits made today
+    $depositSql = "SELECT * FROM Deposits WHERE DATE(timestamp) = CURDATE() AND user_id = $userID AND is_verified = 1";
+    $depositResult = $conn->query($depositSql);
+    $depositArray = $depositResult->fetch_all(MYSQLI_ASSOC);
+
+    $depositSum = array_sum(array_column($depositArray, 'usd_amount'));
+
+    //add dailyApy to all balances, excluding deposits made today
+    $apySql = "UPDATE User SET balance = FLOOR((balance + ($dailyApy / 100 * (balance - $depositSum))) * 100) / 100 WHERE id = $userID";
+    $apyResult = $conn->query($apySql);
+
 }
 
 
 
-$totalApy = (($apyValue + $nexApy) / 2) * 0.85;
-$dailyApy = $totalApy / 365;
 
-//add dailyApy to all balances
-$apySql = "UPDATE User SET balance = FLOOR((balance + ($dailyApy / 100 * balance)) * 100) / 100";
-$apyResult = $conn->query($apySql);
+
+
 
 
 
