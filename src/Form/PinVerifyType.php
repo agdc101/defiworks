1bf143f69f4b6b66ba18f23d1bdc9091038ae0b3
agdc101 <agdc101@gmail.com>
2023-08-06 22:01:38 +0100

    pin form changed to password type and label removed

--- src/Form/PinVerifyType.php
+++ src/Form/PinVerifyType.php
@@ -5,13 +5,19 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class PinVerifyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('pin')
+        $builder->add('pin', PasswordType::class, [
+            'label' => false,
+            'attr' => [
+                'placeholder' => 'Pin',
+                'class' => 'form-control'
+            ]
+        ]);
         ;
     }
 
