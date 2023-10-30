f56b663d2a77f31037663143682ce6296c32dfb6
agdc101 <agdc101@gmail.com>
2023-10-30 08:38:25 +0000

    app flashes changed for registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -61,7 +61,6 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
-                $this->addFlash('success', 'Thank You. Your email address has now been verified.');
 
                 $userAuthenticator->authenticateUser(
                 $user,
