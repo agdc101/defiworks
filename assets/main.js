350034129ab08ef5d607ea15e7594d2941d2fa7f
agdc101 <agdc101@gmail.com>
2023-12-14 08:49:40 +0000

    header fixed to top. js added for bg

--- assets/main.js
+++ assets/main.js
@@ -24,4 +24,14 @@ button.addEventListener('click', function() {
     body.classList.toggle('overflow-hidden');
 });
 
+//when page is scrolled from the top, set background color of header to black
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.style.backgroundColor = 'black';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
 
