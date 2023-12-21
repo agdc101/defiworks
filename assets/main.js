e21fd1d7859508dbfc6ef424c9e815ef43fd01b3
agdc101 <agdc101@gmail.com>
2023-12-21 09:34:27 +0000

    feat: new steps section added to homepage

--- assets/main.js
+++ assets/main.js
@@ -27,8 +27,10 @@ button.addEventListener('click', function() {
 //when page is scrolled from the top, set background color of header to black
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
-    if (window.scrollY > 0) {
+    if (window.scrollY > 0 && window.scrollY < 600) {
         header.style.backgroundColor = 'black';
+    } else if (window.scrollY > 600) {
+        header.style.backgroundColor = '#190426';
     } else {
         header.style.backgroundColor = 'transparent';
     }
