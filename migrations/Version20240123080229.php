d46b52a74894e84c3ed9ebe6a0474be57d85417c
agdc101 <agdc101@gmail.com>
2024-01-23 08:06:28 +0000

    refactor: new cols created in userYieldLog. liveAPY default changed, yield script updated

---
+++ migrations/Version20240123080229.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240123080229 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user_yield_log ADD original_balance DOUBLE PRECISION NOT NULL, ADD ineligible_deposits DOUBLE PRECISION NOT NULL, ADD value_added DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user_yield_log DROP original_balance, DROP ineligible_deposits, DROP value_added');
+    }
+}
