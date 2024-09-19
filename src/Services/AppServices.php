e1314b6fded1c3ba87ad1a4a2dfe333b42a1d3d1
agdc101 <agdc101@gmail.com>
2024-09-19 09:33:06 +0100

    refactor: comments added for appServices funcs

--- src/Services/AppServices.php
+++ src/Services/AppServices.php
@@ -42,8 +42,10 @@ class AppServices
    }
 
    /**
+    * private function updates the apy log table with the new live apy to show on the homepage
+    * @param float $apy
     * @throws RuntimeException
-    */
+   */
    private function updateApyLog($apy) : void {
       try {
          $newApyLog = (new LiveApyLog())
@@ -56,6 +58,10 @@ class AppServices
       }
    }
 
+   /**
+    * private function returns the performance rate based on the apy
+    * @param float $apy
+   */
    private function returnPerformanceRates($apy) {
       $performanceRates = $this->performanceRatesRepository->findAll();
       
@@ -67,6 +73,7 @@ class AppServices
    }  
 
    /**
+    * public function returns the authenticated user or throws an exception if the user is not found (logged in)
     * @throws UserNotFoundException
     */
    public function getUserOrThrowException(): User {
@@ -78,72 +85,85 @@ class AppServices
    }
 
    /**
-   * @throws ServerExceptionInterface
-   * @throws RedirectionExceptionInterface
-   * @throws DecodingExceptionInterface
-   * @throws ClientExceptionInterface
+    * public function returns either the live apy or the yield apy depending on the getLive parameter, as well as the data from the llama API and status code,
+    * an average apy is calculated based on the active pools then a commission rate is determined, then an apy is calculated using the determined commission rate and the average apy
+    * If $getLive is true, the apy is logged in the apy log table
+    * 
+    * @param bool $getLive
+    * 
+    * @throws ServerExceptionInterface
+    * @throws RedirectionExceptionInterface
+    * @throws DecodingExceptionInterface
+    * @throws ClientExceptionInterface
    */
   public function getVaultData($getLive=false): array {
-   $apys = ['nexo' => 9];
-   $responseData = [];
-   $statusCode = null;
+      $apys = ['nexo' => 9];
+      $responseData = [];
+      $statusCode = null;
 
-   // Get all active pools from the database
-   $activePools = $this->poolsRepository->returnAllActivePools();
+      // Get all active pools from the database
+      $activePools = $this->poolsRepository->returnAllActivePools();
 
-   foreach ($activePools as $pool) {
-       $poolId = $pool->getPoolId();
-       $poolName = $pool->getPoolName();
+      foreach ($activePools as $pool) {
+         $poolId = $pool->getPoolId();
+         $poolName = $pool->getPoolName();
 
-      try {
-         $response = $this->client->request('GET', "https://yields.llama.fi/chart/$poolId", ['timeout' => 5]);
-         $statusCode = $response->getStatusCode();
+         try {
+            $response = $this->client->request('GET', "https://yields.llama.fi/chart/$poolId", ['timeout' => 5]);
+            $statusCode = $response->getStatusCode();
 
-         if ($statusCode !== 200) {
-            throw new \RuntimeException('Error fetching data from API');
-         } else {
-            $responseData[$poolName] = $response->toArray()['data'];
-            $liveApy = end($responseData[$poolName])['apy'];
-            $yieldApy = prev($responseData[$poolName])['apy'];
-
-            if ($getLive) {
-               $apys[$poolName] = $liveApy;
+            if ($statusCode !== 200) {
+               throw new \RuntimeException('Error fetching data from API');
             } else {
-               $apys[$poolName] = $yieldApy;
+               $responseData[$poolName] = $response->toArray()['data'];
+               $liveApy = end($responseData[$poolName])['apy'];
+               $yieldApy = prev($responseData[$poolName])['apy'];
+
+               if ($getLive) {
+                  $apys[$poolName] = $liveApy;
+               } else {
+                  $apys[$poolName] = $yieldApy;
+               }
+
+               $pool->setPoolApy($yieldApy);
+               $this->entityManager->persist($pool);
+               $this->entityManager->flush();
             }
-
-            $pool->setPoolApy($yieldApy);
-            $this->entityManager->persist($pool);
-            $this->entityManager->flush();
+         } catch (\RuntimeException $e) {
+            echo "Caught RuntimeException: " . $e->getMessage();
          }
-      } catch (\RuntimeException $e) {
-         echo "Caught RuntimeException: " . $e->getMessage();
       }
+
+      // Calculate the average APY of all pools
+      $averageApy = array_sum($apys) / count($apys);
+      
+      // Determine the commission rate based on the average APY
+      $commission = $this->returnPerformanceRates($averageApy);
+
+      // Calculate the APY using the determined commission rate
+      $avrResponseLiveApy = $averageApy * $commission;
+
+      if ($getLive) {
+         $this->updateApyLog($avrResponseLiveApy);
+      }
+
+      return [
+         'yieldApy' => $avrResponseLiveApy,
+         'responseData' => $responseData,
+         'statusCode' => $statusCode,
+      ];
    }
-
-   // Calculate the average APY of all pools
-   $averageApy = array_sum($apys) / count($apys);
-   
-   // Determine the commission rate based on the average APY
-   $commission = $this->returnPerformanceRates($averageApy);
-
-   // Calculate the live APY using the determined commission rate
-   $avrResponseLiveApy = $averageApy * $commission;
-
-   if ($getLive) {
-      $this->updateApyLog($avrResponseLiveApy);
-   }
-
-   return [
-       'yieldApy' => $avrResponseLiveApy,
-       'responseData' => $responseData,
-       'statusCode' => $statusCode,
-   ];
-}
 
 
    /**
-   * @throws ApyDataException
+    * private function calculates the average apy based on the data and the nexo apy, if a period is provided, the average is calculated based on the period
+    * If period is NULL the average is calculated based on the entire data length
+    * 
+    * @param array $data
+    * @param float $nexoApy
+    * @param int|null $period
+    * 
+    * @throws ApyDataException
    */
    private function calculateAverage($data, $nexoApy, $period=NULL): float {
       $averages = [];
@@ -167,6 +187,12 @@ class AppServices
       return array_sum($averages) / count($averages);
    }
 
+
+   /**
+    * public function returns an array of the average apys based on the data provided,
+    * this is used to display the average apys on the dash, is called from the UpdateStrategyApyCommand
+    * @param array $data
+   */
    public function getAverageApys($data): array {
       $nexoApy = 9;
       $averages = [];
@@ -178,6 +204,11 @@ class AppServices
       return $averages;
    }  
 
+
+   /**
+    * public function that adds a zero to the value if the value has only one decimal place
+    * @param float $value
+   */
    public function addZeroToValue($value) : string {
       $formatUsd = $value;
       if (strlen(substr(strrchr($value, "."), 1)) == 1) {
@@ -186,9 +217,14 @@ class AppServices
       return $formatUsd;
    }
 
+
    /**
+    * public function that builds and persists a transaction to the database
+    * @param object $transaction
+    * @param int $gbp
+    * @param int $usd
+
     * @throws UserNotFoundException
-    * @throws Exception
     */
    public function buildAndPersistTransaction($transaction, $gbp, $usd) : void {
       $user = $this->getUserOrThrowException();
@@ -207,10 +243,17 @@ class AppServices
       $this->entityManager->flush();
    }
 
+
    /**
+    * public function that builds and sends a transaction email to the admin based on the type of transaction
+    * @param string $type
+    * @param object $object
+    * @param string $sc (sort code)
+    * @param string $ac (acc number)
+    *
     * @throws TransportExceptionInterface
     * @throws UserNotFoundException
-    */
+   */
    public function buildAndSendEmail($type, $object, $sc, $ac): void {
       $user = $this->getUserOrThrowException();
       list($firstName, $lastName, $userEmail) = [$user->getFirstName(), $user->getLastName(), $user->getEmail()];
@@ -240,12 +283,11 @@ class AppServices
       $this->mailer->send($email);
    }
 
+
    /**
-    * @throws ServerExceptionInterface
-    * @throws RedirectionExceptionInterface
-    * @throws DecodingExceptionInterface
-    * @throws ClientExceptionInterface|\Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-    */
+    * public function that returns the gecko data from the API - (currently peg for usdc arb)
+    * @throws ClientExceptionInterface
+   */
    public function getGeckoData($api) : array {
       try {
          $response = $this->client->request('GET', $api, ['timeout' => 5]);
@@ -255,6 +297,9 @@ class AppServices
       }
    }
 
+   /**
+    * public function that returns the total TVL of the site
+   */
    public function getSiteTVL() : float {
       $users = $this->userRepository->findAll();
       $totalBalance = 0;
