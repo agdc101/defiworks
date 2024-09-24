53b1d8c1c1e5647f26c134e6fa45bbf2c6973e34
agdc101 <agdc101@gmail.com>
2024-09-24 20:00:22 +0100

    styles: dashboard re-styling. stats added to new react popover comp

--- assets/react/controllers/DashboardPopups.jsx
+++ assets/react/controllers/DashboardPopups.jsx
@@ -6,23 +6,38 @@ import {
   PopoverTrigger,
 } from "./components/popover"
 
-export function DashboardPopups() {
+export function DashboardPopups(props) {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="outline">Open popover</Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80">
-        <div className="grid gap-4">
-            <div className="site-status-info-inner">
-                <p className="site-status" >Average strategy returns:</p>
-                <p>Week: <strong></strong></p>
-                <p>Month: <strong>%</strong></p>
-                <p>Year: <strong>%</strong></p>
+    <>
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant="outline">Strategy Average APY's: </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+                <div className="grid gap-4">
+                    <div className="site-status-info-inner space-y-6">
+                        <p className="font-light">Week APY: <strong className="font-light text-lime-400">{props.weekAvg}%</strong></p>
+                        <p className="font-light">Monthly APY: <strong className="font-light text-lime-400">{props.monthAvg}%</strong></p>
+                        <p className="font-light">Weekly Average APY: <strong className="font-light text-lime-400">{props.yearAvg}%</strong></p>
+                    </div>
+                </div>
+            </PopoverContent>
+            </Popover>
+            <Popover>
+            <PopoverTrigger asChild>
+            <Button variant="outline">DeFi Works Stats: </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+            <div className="grid gap-4">
+                <div className="site-status-info-inner space-y-6">
+                    {/* <p>Total Value Locked: <strong>{% if tvl is null %} $0 {% else  %} ${{ tvl }} {% endif %}</strong></p>
+                    <p>Last Yield APY: <strong>{% if yieldApy is defined %} {{ yieldApy|round(2, 'floor') }}% {% else %} 4.44% {% endif %}</strong></p>
+                    <p>Live APY: <strong>{% if liveApy is defined %} {{ liveApy|round(2, 'floor') }}% {% else %} 4% {% endif %}</strong></p> */}
+                </div>
             </div>
-        </div>
-      </PopoverContent>
-    </Popover>
+            </PopoverContent>
+        </Popover>
+    </>
   )
 }
 
